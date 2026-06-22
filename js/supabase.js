f7c2e2ebeb11b71be81b0d67e38fa9a8bc24b969
DeQerrti <deqerrti@gmail.com>
2026-06-22 13:01:21 +0700

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = 'https://krkuqamtppqttfnhxopp.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_AEyt3613mjc1zUBg1jvdnA_lXzvXtKE';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtya3VxYW10cHBxdHRmbmh4b3BwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIwODQ4MzgsImV4cCI6MjA5NzY2MDgzOH0.UoEj2a0V0a47AQaG54SPzQT99QHHqN3HROycLuF2ToI';
 
 const { createClient } = supabase;
 const db = createClient(SUPABASE_URL, SUPABASE_KEY);
